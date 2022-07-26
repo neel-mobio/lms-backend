871032e68944e36770bc6d5545530146946870a5
Neel <neelp.mobio@gmail.com>
2022-07-26 18:28:28 +0530

    create library apis

--- routes/libraryRouter.js
+++ routes/libraryRouter.js
@@ -1,18 +1,18 @@
-// var express = require('express');
-// var libraryRouter = express.Router();
-// const {
-//     listLibrarys,
-//     libraryDetails,
-//     newLibrary,
-//     updateLibraryDetails,
-//     libraryRemove,
-// } = require('../controllers/Librarys/library');
+var express = require('express');
+var libraryRouter = express.Router();
+const {
+    newLibrary,
+        listLibrarys,
+        libraryDetails,
+        updateLibraryDetails,
+        libraryRemove,
+} = require('../controllers/Librarys/library');
 
 
-// libraryRouter.post('/create',newLibrary);
-// libraryRouter.get('/list',listLibrarys);
-// libraryRouter.get('/:library_id/details',libraryDetails);
-// libraryRouter.put('/:library_id/update',updateLibraryDetails);
-// libraryRouter.delete('/:library_id/delete',libraryRemove);
+libraryRouter.post('/create', newLibrary);
+libraryRouter.get('/list',listLibrarys);
+libraryRouter.get('/:library_id/details',libraryDetails);
+libraryRouter.put('/:library_id/update',updateLibraryDetails);
+libraryRouter.delete('/:library_id/delete',libraryRemove);
 
-// module.exports = libraryRouter;
+module.exports = libraryRouter;
